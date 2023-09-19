1212295a95dccba6971a45f81f9ea7ecafd07be4
Abhishek Dandekar <dandekarabhishek15@gmail.com>
2023-09-19 16:12:42 +0200

    Update infra.mk

--- mk/infra.mk
+++ mk/infra.mk
@@ -12,7 +12,7 @@ infra-atomix: $(M)/atomix
 infra-onos-op: $(M)/onos-operator
 infra-prom-op-servicemonitor: $(M)/prom-op-servicemonitor
 
-$(M)/telemetry: | $(M)/k8s-ready
+$(M)/telemetry: | $(M)/k8s-ready $(M)/helm-ready
 	kubectl apply -f https://raw.githubusercontent.com/cilium/cilium/1.14.2/examples/kubernetes/addons/prometheus/monitoring-example.yaml || true
 	helm install cilium cilium/cilium --version 1.14.2 --namespace kube-system --set prometheus.enabled=true --set operator.prometheus.enabled=true --set hubble.enabled=true --set hubble.metrics.enableOpenMetrics=true --set hubble.metrics.enabled="{dns,drop,tcp,flow:destinationContext=pod-name;sourceContext=pod-name;labelsContext=source_ip\,destination_ip}" --wait || true
 	
